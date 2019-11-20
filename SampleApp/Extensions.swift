c60e3c6c84c5ddacfbab79a3fec51ffa9ebb95c0
Jan Chaloupecky <jan.chaloupecky@gmail.com>
2019-11-20 11:25:01 +0100

    Removed rx swift

--- SampleApp/Extensions.swift
+++ SampleApp/Extensions.swift
@@ -5,43 +5,23 @@
 
 import AVFoundation
 import CoreLocation
-import RxCocoa
-import RxSwift
 import UIKit
 
 extension ViewController {
 
     func showMessage(_ title: String, description: String? = nil) {
         alert(title: title, text: description)
-                .subscribe()
-                .disposed(by: disposeBag)
     }
 
-    func alert(title: String, text: String? = nil) -> Completable {
+    func alert(title: String, text: String? = nil) {
 
-        return Completable.create { observer -> Disposable in
+        let alert = UIAlertController(title: title, message: text, preferredStyle: .alert)
 
-            let alert = UIAlertController(title: title, message: text, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Close", style: .default, handler: { _ in
+            self.dismiss(animated: true, completion: nil)
+        }))
 
-            alert.addAction(UIAlertAction(title: "Close", style: .default, handler: { _ in
-                observer(.completed)
-            }))
-
-            self.present(alert, animated: true, completion: nil)
-
-            return Disposables.create {
-                self.dismiss(animated: true, completion: nil)
-            }
-        }
-    }
-
-    func speak(phrase: String) {
-        print("Speaking: \(phrase)")
-        let synthesizer = AVSpeechSynthesizer()
-        var utterance = AVSpeechUtterance(string: "")
-        utterance = AVSpeechUtterance(string: phrase)
-        utterance.rate = AVSpeechUtteranceDefaultSpeechRate
-        synthesizer.speak(utterance)
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
@@ -51,12 +31,11 @@ extension CLLocationCoordinate2D {
     init?(coordinatesString: String) {
         let stringArray = coordinatesString.split(separator: ",")
         guard
-                stringArray.count == 2,
-                let lat = Double(stringArray[0]),
-                let lon = Double(stringArray[1]) else {
-            return nil
+            stringArray.count == 2,
+            let lat = Double(stringArray[0]),
+            let lon = Double(stringArray[1]) else {
+                return nil
         }
         self.init(latitude: lat, longitude: lon)
     }
-
 }
